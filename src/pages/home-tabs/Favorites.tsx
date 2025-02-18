83a017335acaceecf039429451a53c1458cc353e
LOVELAINEJOYATSIC <tasiclj07@gmail.com>
2025-02-18 08:58:09 +0800

    Tabs setup and /home-tabs/pages updates

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -20,7 +20,18 @@ import {
             <IonTitle>Favorites</IonTitle>
           </IonToolbar>
         </IonHeader>
+
         <IonContent fullscreen>
+<div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Favorites
+        </div>
         </IonContent>
       </IonPage>
     );
