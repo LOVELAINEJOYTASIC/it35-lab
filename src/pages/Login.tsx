3c068b77d855c2bb1612fd13b058157da4d6d8e1
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-03-03 12:58:22 +0000

    added progress bar

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,36 @@
+import React, { useState } from 'react';
 import { 
   IonAvatar,
   IonButton,
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonIcon, 
-    IonInput, 
-    IonInputPasswordToggle, 
-    IonItem, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar, 
-    useIonRouter
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonMenuButton, 
+  IonPage, 
+  IonProgressBar,
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter
 } from '@ionic/react';
 import { logoIonic } from 'ionicons/icons';
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
+  const [loading, setLoading] = useState(false);
 
   const doLogin = () => {
-      navigation.push('/it35-lab/app','forward','replace');
+    setLoading(true);
+    // Simulate a login process
+    setTimeout(() => {
+      setLoading(false);
+      navigation.push('/it35-lab/app', 'forward', 'replace');
+    }, 2000); // Simulate a 2-second login process
   }
+
   return (
     <IonPage>
       <IonHeader>
@@ -30,21 +39,22 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
-      <IonAvatar>
-        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  </IonAvatar>
-      <IonItem>
-  <IonInput type="password" label="Password:" value="" placeholder="">
-  <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-  <IonInput label="Username:" placeholder="" >
-  </IonInput>
-  </IonInput>
-
-</IonItem>
-      </div>
-          <IonButton onClick={() => doLogin()} expand="full">
-              Login
-          </IonButton>
+        {loading && <IonProgressBar type="indeterminate"></IonProgressBar>}
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
+          <IonAvatar>
+            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  
+          </IonAvatar>
+          <IonItem>
+            <IonInput type="password" label="Password:" value="" placeholder="">
+              <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+              <IonInput label="Username:" placeholder="" >
+              </IonInput>
+            </IonInput>
+          </IonItem>
+        </div>
+        <IonButton onClick={() => doLogin()} expand="full">
+          Login
+        </IonButton>
       </IonContent>
     </IonPage>
   );
