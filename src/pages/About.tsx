31d8ea104ec9ec026bd975851a1b5c78d1a37dd2
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-03-03 13:11:41 +0000

    added date and time in about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,28 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const About: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>About</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default About;
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+const About: React.FC = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>About</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', flexDirection: 'column' }}>
+          <div style={{ marginBottom: '20px' }}>
+            {dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}
+          </div>
+
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default About;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,27 @@
-import React, { useState } from 'react';
 import { 
   IonAvatar,
   IonButton,
   IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonIcon, 
-  IonInput, 
-  IonInputPasswordToggle, 
-  IonItem, 
-  IonMenuButton, 
-  IonPage, 
-  IonProgressBar,
-  IonTitle, 
-  IonToolbar, 
-  useIonRouter
+    IonContent, 
+    IonHeader, 
+    IonIcon, 
+    IonInput, 
+    IonInputPasswordToggle, 
+    IonItem, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
 } from '@ionic/react';
 import { logoIonic } from 'ionicons/icons';
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
-  const [loading, setLoading] = useState(false);
 
   const doLogin = () => {
-    setLoading(true);
-    // Simulate a login process
-    setTimeout(() => {
-      setLoading(false);
-      navigation.push('/it35-lab/app', 'forward', 'replace');
-    }, 2000); // Simulate a 2-second login process
+      navigation.push('/it35-lab/app','forward','replace');
   }
-
   return (
     <IonPage>
       <IonHeader>
@@ -39,22 +30,21 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        {loading && <IonProgressBar type="indeterminate"></IonProgressBar>}
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
-          <IonAvatar>
-            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  
-          </IonAvatar>
-          <IonItem>
-            <IonInput type="password" label="Password:" value="" placeholder="">
-              <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-              <IonInput label="Username:" placeholder="" >
-              </IonInput>
-            </IonInput>
-          </IonItem>
-        </div>
-        <IonButton onClick={() => doLogin()} expand="full">
-          Login
-        </IonButton>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
+      <IonAvatar>
+        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  </IonAvatar>
+      <IonItem>
+  <IonInput type="password" label="Password:" value="" placeholder="">
+  <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+  <IonInput label="Username:" placeholder="" >
+  </IonInput>
+  </IonInput>
+
+</IonItem>
+      </div>
+          <IonButton onClick={() => doLogin()} expand="full">
+              Login
+          </IonButton>
       </IonContent>
     </IonPage>
   );
