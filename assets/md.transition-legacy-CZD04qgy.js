fbc6dd2038291d64d07aec1e497f74f229f34956
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-03-03 13:42:03 +0000

    Updates

--- assets/md.transition-legacy-CZD04qgy.js
+++ assets/md.transition-legacy-CZD04qgy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DOpqx1pB.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-esYu-zTa.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
