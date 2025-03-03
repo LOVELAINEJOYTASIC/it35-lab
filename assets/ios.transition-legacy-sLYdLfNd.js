6e386731638ec2b6c69bb99b2af21f57be03a510
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-03-03 13:23:13 +0000

    Updates

--- assets/ios.transition-legacy-sLYdLfNd.js
+++ assets/ios.transition-legacy-sLYdLfNd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DZ6es7Od.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DOpqx1pB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
