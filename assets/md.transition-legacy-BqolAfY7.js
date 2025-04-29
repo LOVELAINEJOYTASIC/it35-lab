179ee37cab2e06e109a4ef02f090c7031d0b520a
LOVELAINEJOYTASIC <tasiclj07@gmail.com>
2025-04-29 08:57:54 +0800

    Updates

--- assets/md.transition-legacy-BqolAfY7.js
+++ assets/md.transition-legacy-BqolAfY7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLhth1iv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BiibS2SJ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
